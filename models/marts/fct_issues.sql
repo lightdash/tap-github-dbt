61cbe21601c14bfb292a0bcd136bb705ec87e33e
Katie Hindson <hindsonk@gmail.com>
2021-07-09 17:33:20 +0200

    changes to postgres date diff

--- models/marts/fct_issues.sql
+++ models/marts/fct_issues.sql
@@ -11,5 +11,6 @@ SELECT
     created_at,
     updated_at,
     pull_request_id,
-    user_id
+    user_id,
+    IF(closed_at IS NOT NULL, DATE_PART('day', closed_at - created_at), NULL) AS num_days_between_issue_created_and_closed
 FROM {{ ref('stg_github__issues') }}
--- models/marts/fct_pull_requests.sql
+++ models/marts/fct_pull_requests.sql
@@ -11,5 +11,6 @@ SELECT
     merged_at,
     closed_at,
     created_at,
-    updated_at
-  FROM {{ ref('stg_github__pull_requests') }}
+    updated_at,
+    IF(merged_at IS NOT NULL, DATE_PART('day', merged_at - created_at), NULL) AS num_days_between_pull_request_created_and_merged
+FROM {{ ref('stg_github__pull_requests') }}
