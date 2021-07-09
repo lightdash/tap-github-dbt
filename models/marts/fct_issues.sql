f20c3492c8c1bef6c0068e5f0181bec09f3920e3
Katie Hindson <hindsonk@gmail.com>
2021-07-09 17:24:16 +0200

    changes SQL from BigQuery to PostGres friendly

--- models/marts/fct_issues.sql
+++ models/marts/fct_issues.sql
@@ -6,13 +6,11 @@ SELECT
     repository,
     number,
     closed_at,
-    TIMESTAMP_TRUNC(TIMESTAMP(closed_at), WEEK, 'UTC') AS closed_at_week,
     title,
     body,
     created_at,
-    TIMESTAMP_TRUNC(TIMESTAMP(created_at), WEEK, 'UTC') AS created_at_week,
     updated_at,
     pull_request_id,
     user_id,
-    IF(closed_at IS NOT NULL, DATE_DIFF(closed_at, created_at, DAY), NULL) AS num_days_between_issue_created_and_closed
+    IF(closed_at IS NOT NULL, DATEDIFF(DAY, closed_at, created_at), NULL) AS num_days_between_issue_created_and_closed
 FROM {{ ref('stg_github__issues') }}
