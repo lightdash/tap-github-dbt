cd7e970029513a225ef2bb573d843604676088b4
Katie Hindson <hindsonk@gmail.com>
2021-07-13 12:19:54 +0200

    updates date parts included in tables

--- models/marts/fct_pull_requests.sql
+++ models/marts/fct_pull_requests.sql
@@ -12,5 +12,9 @@ SELECT
     closed_at,
     created_at,
     updated_at,
+    merged_at::date AS merged_at_date,
+    closed_at::date AS closed_at_date,
+    created_at::date AS created_at_date,
+    updated_at::date AS updated_at_date,
     EXTRACT(day FROM merged_at - created_at) AS num_days_between_pull_request_created_and_merged
 FROM {{ ref('stg_github__pull_requests') }}
