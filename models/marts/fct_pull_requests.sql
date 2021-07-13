815040ad4e3ccf3d35862ef4722d6ac6e377f0f6
Katie Hindson <hindsonk@gmail.com>
2021-07-13 11:45:25 +0200

    adds day extract

--- models/marts/fct_pull_requests.sql
+++ models/marts/fct_pull_requests.sql
@@ -12,5 +12,5 @@ SELECT
     closed_at,
     created_at,
     updated_at,
-    merged_at - created_at AS num_days_between_pull_request_created_and_merged
+    EXTRACT(day FROM merged_at - created_at) AS num_days_between_pull_request_created_and_merged
 FROM {{ ref('stg_github__pull_requests') }}
